f6f6ca5439cfd592012ed8f58f553cf4a32937b7
Vlad Marchuk <marchukvl02@gmail.com>
2025-08-07 13:29:55 +0300

    5 nodes

--- scripts/init-database.sql
+++ scripts/init-database.sql
@@ -8,6 +8,14 @@ WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'chainlink_node_1')\ge
 SELECT 'CREATE DATABASE chainlink_node_2'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'chainlink_node_2')\gexec
 
+SELECT 'CREATE DATABASE chainlink_node_3'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'chainlink_node_3')\gexec
+
+SELECT 'CREATE DATABASE chainlink_node_4'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'chainlink_node_4')\gexec
+
+SELECT 'CREATE DATABASE chainlink_node_5'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'chainlink_node_5')\gexec
 -- Add more databases as needed
 -- SELECT 'CREATE DATABASE chainlink_node_3'
 -- WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'chainlink_node_3')\gexec
