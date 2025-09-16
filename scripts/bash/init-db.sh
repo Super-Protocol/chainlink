ffbabb6e0ac75155d1f3d96dd8ca949e75e12c74
Dmitry Somov <dmitry.somov@superprotocol.com>
2025-09-16 18:29:28 +0200

    refactor: update init-db.sh (SP-6843)

--- scripts/bash/init-db.sh
+++ scripts/bash/init-db.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 set -e
 
-TOTAL_NODES="${1:-5}" # Принимаем количество нод из аргумента, по умолчанию 5
+TOTAL_NODES="${1:-5}" # Accepting the number of nodes from the argument, default is 5
 log() { echo "[init-db] $*"; }
 
-# Переменные из env, можно переопределить
+# Variables from env, can be overridden
 APP_DB_USER="${PGUSER:-chainlink}"
 APP_DB_PASS="${PGPASSWORD:-chainlinkchainlink}"
 
-# Инициализируем кластер БД, если директория данных пуста
+# Initializing the database cluster, if the data directory is empty
 if [ -z "$(ls -A "$PGDATA")" ]; then
     log "Initializing PostgreSQL database as user 'postgres'..."
-    # ИСПРАВЛЕНИЕ: Запускаем initdb от имени пользователя postgres
     su - postgres -c "initdb -D \"$PGDATA\" --username=\"$POSTGRES_USER\""
 fi
 
-# Запускаем Postgres в фоновом режиме для выполнения команд
+# Starting Postgres in the background for executing commands
 log "Starting temporary PostgreSQL server as user 'postgres'..."
-# ИСПРАВЛЕНИЕ: Запускаем сам сервер от имени пользователя postgres
 su - postgres -c "postgres -D \"$PGDATA\"" &
 pid="$!"
 
-# Ждем, пока сервер будет готов принимать подключения
+# Waiting for the server to be ready to accept connections
 until su - postgres -c "pg_isready -h localhost -p 5432 -U \"$POSTGRES_USER\""; do
   log "Waiting for PostgreSQL to start..."
   sleep 1
 done
 log "PostgreSQL is ready."
 
-# Создаем пользователя для нод Chainlink, если его нет
-# ИСПРАВЛЕНИЕ: Выполняем psql-запросы от имени пользователя postgres
+# Creating the user for Chainlink nodes, if it doesn't exist
 if ! su - postgres -c "psql -t -c \"SELECT 1 FROM pg_roles WHERE rolname='$APP_DB_USER'\"" | grep -q 1; then
     log "Creating user $APP_DB_USER..."
     su - postgres -c "psql -c \"CREATE USER \\\"$APP_DB_USER\\\" WITH PASSWORD '$APP_DB_PASS';\""
@@ -37,10 +34,9 @@ else
     log "User $APP_DB_USER already exists."
 fi
 
-# Создаем базы данных для каждой ноды
+# Creating databases for each node
 for i in $(seq 1 "$TOTAL_NODES"); do
     DB_NAME="chainlink_node_${i}"
-    # ИСПРАВЛЕНИЕ: Выполняем psql-запросы от имени пользователя postgres
     if su - postgres -c "psql -lqt | cut -d \| -f 1 | grep -qw \"$DB_NAME\""; then
         log "Database $DB_NAME already exists."
     else
@@ -49,7 +45,7 @@ for i in $(seq 1 "$TOTAL_NODES"); do
     fi
 done
 
-# Останавливаем временный процесс Postgres
+# Stopping the temporary Postgres process
 log "Stopping temporary PostgreSQL server..."
 kill -SIGINT "$pid"
 wait "$pid" || true
