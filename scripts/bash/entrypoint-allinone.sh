936048c9bf526ebce850b98103dfba6acf3e5fdb
Lex Kheben <i@zbit.name>
2025-09-23 12:53:41 +0300

    refactor(entrypoint-allinone): remove unused environment variable exports for cleaner configuration

--- scripts/bash/entrypoint-allinone.sh
+++ scripts/bash/entrypoint-allinone.sh
@@ -50,13 +50,10 @@ export ALL_IN_ONE="true"
 export NODE_NUMBER=${i}
 export CHAINLINK_ROOT=/chainlink
 export NODE_ROOT_DIR=/chainlink/node-${i}
-export MANAGE_POSTGRES=false
 export CHAINLINK_WEB_SERVER_HTTP_PORT=${API_PORT}
 export API_PORT=${API_PORT}
 export P2P_PORT=${P2P_PORT}
 export CONFIGURATION_PUBLIC_KEY="MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAE1mXRd/v32RmPknpTnasAa3b5G31lbOUwMV4cQK/GU8WHySSvJj1MSCsdwhGggGoroMD2Qp/Ql2UOAiGvDRDmGw=="
-export CHAINLINK_EMAIL=admin@example.com
-export CHAINLINK_PASSWORD=yoursuperpassword
 cd /scripts
 exec node index.js
 EOF
